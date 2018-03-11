fe7032b2f172ab1d3b4e568769a275b01da1cbdd
Pierre du Plessis <pierre@pcservice.co.za>
2018-03-11 20:28:30 +0200

    Add Bundle class

---
+++ src/SimpleResponseBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SimpleResponseBundle project.
+ *
+ * @author     Pierre du Plessis
+ * @copyright  Copyright (c) SolidWorx <open-source@solidworx.co>
+ */
+
+namespace SolidWorx\SimpleResponseBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SimpleResponseBundle extends Bundle
+{
+}
